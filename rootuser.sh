d8b725d6321e8f2c44b3737386f17670e3db3dba
kanalavinodkumar <kanalavinodkumar@gmail.com>
2024-08-19 06:22:45 +0530

    check root user

--- rootuser.sh
+++ rootuser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $(id -u) -ne 0 ]; then
-	echo "$(id) is not Root user"
+	echo "$(id -un) is not Root user"
 else
-	echo "$(id) is Root user"
+	echo "$(id -un)is Root user"
 fi
---
+++ rootuser.sh.bak
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+	echo "$(id) is not Root user"
+else
+	echo "$(id) is Root user"
+fi
